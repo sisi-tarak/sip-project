0f4bad1c0ebc3953749ecdad91b9f9e636f001da
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2024-11-10 19:03:17 +0530

    responsive explain 1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import Img1 from "../assets/react.svg";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="flex justify-center gap-10 text-6xl items-center h-16 text-gray-900 ">
-        About Page
-      </h1>
+    <div className="container-fluid bg-gray-800 text-white">
+      <div className="flex flex-col justify-items-center align-middle gap-16 p-20 text-center md:flex-row ">
+        <div className="flex flex-col align-middle">
+          <h1>Build Your Next Project</h1>
+          <p>
+            Create, innovate, and transform your ideas into reality with our
+            powerful tools and solutions.
+          </p>
+
+          <div className="flex justify-center gap-12">
+            <button className="bg-white text-gray-800 px-6 py-4 rounded text-lg">
+              Start Free Trial
+            </button>
+            <button>Watch Demo</button>
+          </div>
+        </div>
+
+        <div>
+          <img src={Img1} alt="Hero Image" className="w-72 h-auto mx-auto" />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>Build Your Next Project</h1>
+        <p>Create, innovate, and transform your ideas into reality with our powerful tools and solutions.</p>
+        
+        <div>
+          <button>Start Free Trial</button>
+          <button>Watch Demo</button>
+        </div>
+      </div>
+
+      <div>
+        <img 
+          src="/api/placeholder/500/300" 
+          alt="Hero Image"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
